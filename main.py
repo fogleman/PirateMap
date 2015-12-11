5febd363653dcac0b6b0e762ea17b19ee100df07
Michael Fogleman <fogleman@gmail.com>
2015-12-10 22:01:22 -0500

    sweet layers

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from poisson_disc import poisson_disc
 from shapely.geometry import Polygon, MultiPolygon
 from shapely.affinity import translate
 import cairo
+import layers
 import math
 import noise
 import random
@@ -14,57 +15,48 @@ def hex_color(dc, color):
     b = int(color[4:6], 16) / 255.0
     dc.set_source_rgb(r, g, b)
 
-def filter_points(points, levels):
-    dx = random.random() * 10000
-    dy = random.random() * 10000
-    result = []
-    for _ in levels:
-        result.append([])
-    for x, y in points:
-        d = math.hypot(x - 256, y - 256)
-        p = d / 256
-        m = 0.005
-        v = noise.snoise2(dx + x * m, dy + y * m, 8)
-        for i, level in enumerate(levels):
-            if v - p > level:
-                result[i].append((x, y))
-        # if v - p > -0.5:
-        #     result.append((x, y))
-    return result
+def make_layer():
+    x = layers.Noise(8).add(layers.Constant(0.6)).clamp()
+    x = x.translate(random.random() * 1000, random.random() * 1000)
+    x = x.scale(0.005, 0.005)
+    x = x.subtract(layers.Distance(256, 256, 256))
+    return x
 
 def render_shape(dc, shape):
     if isinstance(shape, MultiPolygon):
         for child in shape.geoms:
             render_shape(dc, child)
     if isinstance(shape, Polygon):
-        # TODO: interior
         dc.new_sub_path()
         for x, y in shape.exterior.coords:
             dc.line_to(x, y)
         dc.close_path()
-        for interior in shape.interiors:
-            print interior
-            render_shape(dc, interior)
+        # TODO: interior
+        # for interior in shape.interiors:
+        #     render_shape(dc, interior)
 
 def main():
     width = height = 512
-    surface = cairo.ImageSurface(cairo.FORMAT_RGB24, width, height)
+    scale = 2
+    surface = cairo.ImageSurface(cairo.FORMAT_RGB24, width * scale, height * scale)
     dc = cairo.Context(surface)
+    dc.scale(scale, scale)
     dc.set_fill_rule(cairo.FILL_RULE_EVEN_ODD)
     hex_color(dc, '2185C5')
     dc.paint()
+    layer = make_layer()
+    layer.save('layer.png', 0, 0, width, height)
     points = poisson_disc(0, 0, width, height, 8, 32)
-    points1, points2 = filter_points(points, [-0.5, -0.25])
-    shape = alpha_shape(points1, 0.1).buffer(-4).buffer(4)
-    shape2 = alpha_shape(points2, 0.1).buffer(-6).buffer(6)
+    shape1 = layer.alpha_shape(points, 0.1, 0.1).buffer(-4).buffer(4)
+    shape2 = layer.alpha_shape(points, 0.2, 0.1).buffer(-8).buffer(4)
     hex_color(dc, '7ECEFD')
-    render_shape(dc, shape.simplify(8).buffer(64).buffer(-32))
+    render_shape(dc, shape1.simplify(8).buffer(64).buffer(-32))
     dc.fill()
     hex_color(dc, 'BDD4DE')
-    render_shape(dc, shape.buffer(4))
+    render_shape(dc, shape1.buffer(4))
     dc.fill()
     hex_color(dc, 'FFFFA6')
-    render_shape(dc, shape)
+    render_shape(dc, shape1)
     dc.fill()
     hex_color(dc, 'BDF271')
     render_shape(dc, shape2)
